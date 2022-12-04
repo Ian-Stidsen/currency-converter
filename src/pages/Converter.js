741b1228a0d18da0e4a564b0b2d8d6d4af8bb79e
Ian Stidsen <ianlundstidsen@gmail.com>
2022-12-04 16:19:55 -0300

    downloads the entire API rates

--- src/pages/Converter.js
+++ src/pages/Converter.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, {
+  useEffect,
+} from 'react';
 import '../stylesheets/converter.css';
 
 import API_KEYS from '../data/API.json';
 
+//const CURRENCY_API =`https://api.apilayer.com/currency_data/convert?to=${currencyTo.value}&from=${currencyFrom.value}&amount=${inputFrom.value}`;
+
 
 function Converter() {
-  let inputFrom;
-  let inputTo;
-  let currencyFrom;
-  let currencyTo;
 
-  window.addEventListener('load', () => {
-    inputFrom = document.getElementById('convertFrom');
-    inputTo = document.getElementById('convertTo');
-    currencyFrom = document.getElementById('currencyFrom');
-    currencyTo = document.getElementById('currencyTo');
-  });
+  let conversionRates;
 
-  function convert () {
-    const CURRENCY_API =`https://api.apilayer.com/currency_data/convert?to=${currencyTo.value}&from=${currencyFrom.value}&amount=${inputFrom.value}`;
+  // Runs the API once when the page is loaded.
+  useEffect(() => {
+    getConversionRates();
+  })
+
+  function getConversionRates () {
+    const CURRENCY_API = 'https://api.apilayer.com/currency_data/live?base=USD&symbols=EUR,GBP';
 
     let myHeaders = new Headers();
     myHeaders.append("apikey", API_KEYS.Converter_APIKEY);
@@ -32,22 +32,53 @@ function Converter() {
     function getConversion () {
       return fetch(CURRENCY_API, requestOptions)
         .then(response => response.json())
-        .then(data => data.result);
+        .then(data => data.quotes);
     }
 
     async function responseHandler () {
       const response = await getConversion();
-      inputTo.value = response;
-      console.log()
+      conversionRates = response;
+      console.log(conversionRates);
     }
 
     responseHandler()
-  }
+  };
+
+  function convert() {
+    const inputFrom = document.getElementById('convertFrom');
+    const inputTo = document.getElementById('convertTo');
+    const currencyFrom = document.getElementById('currencyFrom');
+    const currencyTo = document.getElementById('currencyTo');
+
+    // If you convert from the same currency it just returns the same value.
+    if (currencyFrom.value === currencyTo.value) {
+      inputTo.value = inputFrom.value;
+      return;
+    };
+    
+    const rate = () => {
+      let from;
+      let to;
+
+      // Since the base currency from the API is USD i just set the value to 1:
+      if (currencyFrom.value === 'USD') from = 1;
+      else from = conversionRates['USD' + currencyFrom.value];
+
+      if (currencyTo.value === 'USD') to = 1;
+      else to = conversionRates['USD' + currencyTo.value];
+
+      // Calculates to convertion rate by dividing the currency values compared to USD.
+      return to / from;
+    }  
+    
+    inputTo.value = rate() * inputFrom.value; 
+  };
+
   return (
     <div className="container">
       <h1 className='converter-title'>Currency Converter</h1>
       <form>
-        <label htmlFor="convertFrom">Convert from</label>
+
         <div className="input-group">
           <div className="input-group-text">
             <select className="form-select" onChange={convert} name="currency" id="currencyFrom">
@@ -55,14 +86,13 @@ function Converter() {
               <option value="USD">USD</option>
               <option value="EUR">EUR</option>
               <option value="GBP">GPB</option>
-
             </select>
           </div>
-          <input className="form-control" onChange={convert} type="number" id="convertFrom" placeholder="Amount"></input>
-        </div>
+          <div className='form-floating'>
+            <input className="form-control" onChange={convert} type="number" id="convertFrom" placeholder="Amount"></input>
+            <label htmlFor="convertFrom" className='form-label'>Convert from</label>
+          </div>
 
-        <label htmlFor="convertTo">Convert to</label>
-        <div className="input-group">
           <div className="input-group-text">
             <select className="form-select" onChange={convert} name="currency" id="currencyTo">
               <option value="DKK">DKK</option>
@@ -70,8 +100,12 @@ function Converter() {
               <option value="EUR">EUR</option>
             </select>
           </div>
-          <input disabled className="form-control" type="number" id="convertTo" placeholder="Amount"></input>
+          <div className='form-floating'>
+            <input disabled className="form-control" type="number" id="convertTo" placeholder=""></input>
+            <label htmlFor="convertTo" className='form-label'>Convert to</label>
+          </div>
         </div>
+
       </form>
     </div>
   );
